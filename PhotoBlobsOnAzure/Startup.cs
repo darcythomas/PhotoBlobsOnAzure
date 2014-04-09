922410cf72eac935b3acad1bbbc3c100304d803e
darcythomas <github@darcy.io>
2014-04-09 21:23:18 +1200

    Added new (newer ver) MVC code

---
+++ PhotoBlobsOnAzure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoBlobsOnAzure.Startup))]
+namespace PhotoBlobsOnAzure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
